d40cc08591e7504c20a3c2769190ba43757f23d1
Chris <30360237+ccaruvana@users.noreply.github.com>
2017-08-12 18:52:10 -0500

    Updated decrypt.py
    Added option to output decrypted text to a file

--- decrypt.py
+++ decrypt.py
@@ -9,6 +9,19 @@ while (not os.path.exists(key)):
 	print("Whoops! That file doesn't seem to exist.")
 	key = input("Please enter the decryption key: ")
 
+newFile = input("Would you like to output the decrypted text to a file? [y/n]: ")
+while (newFile != "y" and newFile != "n"):
+	newFile = input("Plese input a valid choice: y-yes, n-no: ")
+if (newFile == "y"):
+	override = 0
+	while (override != '1'):
+		outFile = input("Please input the name of the file you wish to write to: ")
+		if (os.path.exists(outFile)):
+			print("It seems that this file already exists. Do you wish to overwrite it?")
+			override = input("'1' for overwrite | '0' to input new file name: ")
+		else:
+			override = str(1)
+
 fA = open(encrypted,"r")
 fB = open(key,"r")
 encF = fA.readlines()
@@ -35,4 +48,9 @@ else:
 		decrypted.append(chr(encArr[k]^keyArr[k]))
 	decrypted = ''.join(decrypted)
 
-	print(decrypted)
+	if (newFile == "y"):
+		output = open(outFile, "w")
+		output.write(decrypted)
+		output.close()
+	else:
+		print(decrypted)
